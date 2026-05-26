a71b356b23cd78e3a2f6147726806c5bc4e87a8d
George-zz3 <18928476586@163.com>
2026-05-27 02:07:43 +0800

    上传完整工具类项目

---
+++ src/main/java/io/github/RabbitUtilTest.java
@@ -0,0 +1,9 @@
+package io.github;
+
+import io.github.george_zx77.RabbitUtil;
+
+public class RabbitUtilTest {
+    public static void main(String[] args) {
+        RabbitUtil.xxx();
+    }
+}
---
+++ src/main/java/io/github/george_zx77/RabbitUtil.java
@@ -0,0 +1,7 @@
+package io.github.george_zx77;
+
+public class RabbitUtil {
+    public static void xxx(){
+        System.out.println("wow");
+    }
+}
